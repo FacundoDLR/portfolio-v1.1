f5fc294d0712a0bb18c2b880e7826e04742f0dbf
Facundo DE LOS RIOS <facujofre96@gmail.com>
2023-01-09 22:02:46 -0300

    Modificacinoes CSS y HTML de la seccion de formulario de contacto y agregue funcionalidad para enviar mensaje a mi casilla de correo

---
+++ assets/js/contact_form.js
@@ -0,0 +1,34 @@
+((d) => {
+    const $form = d.querySelector(".contact-form");
+    const $loader = d.querySelector(".contact-form-loader");
+    const $response = d.querySelector(".contact-form-response");
+
+    $form.addEventListener("submit", (e) => {
+
+        e.preventDefault();
+        $loader.classList.remove("none");
+        fetch("https://formsubmit.co/ajax/facujofre96@gmail.com",{
+           method: "POST",
+           body: new FormData(e.target) 
+        })
+        .then( (res) => (res.ok ? res.json() : Promise.reject(res)))
+        .then( (json) => {
+            console.log(json)
+            location.hash = "#gracias";
+            $form.reset();
+        })
+        .catch( (err) => {
+            let message = err.statusText || "Ocurrio un error";
+            $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`;
+        })
+        .finally(() => {
+            $loader.classList.add("none");
+            setTimeout(() => {
+                location.hash = "#close"
+            }, 3000)
+        })
+
+    })
+
+
+})(document)
